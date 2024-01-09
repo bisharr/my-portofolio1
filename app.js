6a82969ce7e26abc463bf83f6aedb41bca7fa6a3
bisharr <bishar995@gmail.com>
2024-01-09 21:19:20 +0300

    Some updates

--- app.js
+++ app.js
@@ -11,14 +11,29 @@ function closemenu() {
 
 const icon = document.querySelector('.icon');
 const logo = document.getElementById('logo');
+const serviceEl = document.querySelectorAll('.services-list-content');
+const inputEl = document.querySelectorAll('.form-input');
 
 icon.addEventListener('click', function () {
   document.body.classList.toggle('dark-Theme');
   if (document.body.classList.contains('dark-Theme')) {
+    for (let i = 0; i < serviceEl.length; i++) {
+      serviceEl[i].style.backgroundColor = '#080808e1';
+      serviceEl[i].style.color = '#e2d9d9f3';
+      inputEl[i].style.backgroundColor = '#080808e1';
+      inputEl[i].style.color = '#e2d9d9f3';
+    }
+
     icon.src = 'moon.png';
     logo.src = 'images/logo-2.jpg';
   } else {
     icon.src = 'sun.png';
     logo.src = 'images/logo-1.jpg';
+    for (let i = 0; i < serviceEl.length; i++) {
+      serviceEl[i].style.backgroundColor = '#e2d9d9f3';
+      serviceEl[i].style.color = '#080808e1';
+      inputEl[i].style.backgroundColor = '#e2d9d9f3';
+      inputEl[i].style.color = '#080808e1';
+    }
   }
 });
